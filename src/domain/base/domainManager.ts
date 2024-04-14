58cea22d59b81445df39a19682d232f208b4b553
Dongkey-Dev <dongkeydev@gmail.com>
2024-04-14 13:34:33 +0900

    사용하지 않는 추상 메서드 삭제

--- src/domain/base/domainManager.ts
+++ src/domain/base/domainManager.ts
@@ -8,14 +8,9 @@ export abstract class DomainManager<T> {
   }
   protected abstract sendToDatabase(toDomain: T): Promise<boolean>;
   protected abstract getFromDatabase(entityKey: any, condition?): Promise<T>;
-  protected abstract getListFromDatabase(entityKey: any, condition?);
 
   getDomain(entityKey: any): Promise<T> {
     return this.getFromDatabase(entityKey);
-  }
-
-  getDomainList(entityKey: any): Promise<T[]> {
-    return this.getListFromDatabase(entityKey);
   }
 
   applyDomain(domain: T): Promise<boolean> {
